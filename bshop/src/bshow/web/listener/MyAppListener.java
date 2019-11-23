b4d3548d08d9cd58444841d4af64bd0da4398ee0
duanjuntang <814402195@qq.com>
2019-11-23 15:04:09 +0800

    我添加了监听

---
+++ bshop/src/bshow/web/listener/MyAppListener.java
@@ -0,0 +1,40 @@
+package bshow.web.listener;
+
+import java.io.InputStream;
+
+import java.util.Properties;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
+public class MyAppListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("程序启动了");
+		//获取config.xml文件
+		InputStream config=MyAppListener.class.getResourceAsStream("/bshop/mvc/config.xml");
+		System.out.println(config);
+		SAXReader reader=new SAXReader();
+		Document doc=null;
+		//准备存Action实例的map集合
+		Properties actionPool=new Properties();
+		try {
+			doc=reader.read(config);
+			//将配置文件存在应用程序的作用域中
+			sce.getServletContext().setAttribute("doc", doc);
+			sce.getServletContext().setAttribute("actionPool", actionPool);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+	}
+}

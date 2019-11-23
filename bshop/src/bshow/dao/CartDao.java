445155d2c169bfa5b30d39172882e91c531f0be3
JaLyJoker <1041551225@qq.com>
2019-11-23 14:49:15 +0800

    添加购物车dao和impl的架子

---
+++ bshop/src/bshow/dao/CartDao.java
@@ -0,0 +1,25 @@
+package bshow.dao;
+
+import java.sql.Connection;
+import java.util.List;
+
+import bshow.pojo.Cart_table;
+
+public interface CartDao {
+	
+	//增加购物车内的商品
+	public boolean insertCart(Cart_table cart,Connection con) throws Exception;
+	
+	//删除购物车内的商品
+	public boolean deleteCart(Cart_table cart,Connection con) throws Exception; 
+	
+	//更新购物车内的商品
+	public boolean updateCart(Cart_table cart,Connection con) throws Exception;
+	
+	//查找购物车商品
+	public List<Cart_table> queryAllCart(Connection con) throws Exception;
+	
+	
+	
+	
+}
---
+++ bshop/src/bshow/dao/impl/CartDaoImpl.java
@@ -0,0 +1,37 @@
+package bshow.dao.impl;
+
+import java.sql.Connection;
+import java.util.List;
+
+import bshow.dao.CartDao;
+import bshow.pojo.Cart_table;
+
+public class CartDaoImpl implements CartDao{
+
+	@Override
+	public boolean insertCart(Cart_table cart, Connection con) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteCart(Cart_table cart, Connection con) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateCart(Cart_table cart, Connection con) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Cart_table> queryAllCart(Connection con) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}

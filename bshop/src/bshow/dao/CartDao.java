fb4943c2472885894da0fb32103a9f9acaa2099f
Administrator <Administrator@PC-PJ0801>
2019-11-24 16:34:44 +0800

    实现类写了查找购物车的sql

--- bshop/src/bshow/dao/CartDao.java
+++ bshop/src/bshow/dao/CartDao.java
@@ -6,20 +6,17 @@ import java.util.List;
 import bshow.pojo.Cart_table;
 
 public interface CartDao {
-	
-	//增加购物车内的商品
-	public boolean insertCart(Cart_table cart,Connection con) throws Exception;
-	
-	//删除购物车内的商品
-	public boolean deleteCart(Cart_table cart,Connection con) throws Exception; 
-	
-	//更新购物车内的商品
-	public boolean updateCart(Cart_table cart,Connection con) throws Exception;
-	
-	//查找购物车商品
+
+	// 增加购物车内的商品
+	public boolean insertCart(Cart_table cart, Connection con) throws Exception;
+
+	// 删除购物车内的商品
+	public boolean deleteCart(Cart_table cart, Connection con) throws Exception;
+
+	// 更新购物车内的商品
+	public boolean updateCart(Cart_table cart, Connection con) throws Exception;
+
+	// 查找购物车商品
 	public List<Cart_table> queryAllCart(Connection con) throws Exception;
-	
-	
-	
-	
+
 }

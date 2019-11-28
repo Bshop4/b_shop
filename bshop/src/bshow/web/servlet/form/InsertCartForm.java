303771b4d9986ccd9254e6eef9482e42752945cc
JaLyJoker <1041551225@qq.com>
2019-11-28 10:19:53 +0800

    创建了insertcartform

---
+++ bshop/src/bshow/web/servlet/form/InsertCartForm.java
@@ -0,0 +1,87 @@
+package bshow.web.servlet.form;
+
+import bshow.web.servlet.core.ActionForm;
+
+public class InsertCartForm extends ActionForm{
+	
+	private String msg;
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+//	private String goodsNo;
+//	private String getnumber;
+//	private String imgurl;
+//	private String getgoodsname;
+//	private String getprice;
+//	private String getAllprice;
+//	private String getcolor;
+//	private String getsize;
+//	private String state;
+//	
+//	public String getGoodsNo() {
+//		return goodsNo;
+//	}
+//	public void setGoodsNo(String goodsNo) {
+//		this.goodsNo = goodsNo;
+//	}
+//	public String getGetnumber() {
+//		return getnumber;
+//	}
+//	public void setGetnumber(String getnumber) {
+//		this.getnumber = getnumber;
+//	}
+//	public String getImgurl() {
+//		return imgurl;
+//	}
+//	public void setImgurl(String imgurl) {
+//		this.imgurl = imgurl;
+//	}
+//	public String getGetgoodsname() {
+//		return getgoodsname;
+//	}
+//	public void setGetgoodsname(String getgoodsname) {
+//		this.getgoodsname = getgoodsname;
+//	}
+//	public String getGetprice() {
+//		return getprice;
+//	}
+//	public void setGetprice(String getprice) {
+//		this.getprice = getprice;
+//	}
+//	public String getGetAllprice() {
+//		return getAllprice;
+//	}
+//	public void setGetAllprice(String getAllprice) {
+//		this.getAllprice = getAllprice;
+//	}
+//	public String getGetcolor() {
+//		return getcolor;
+//	}
+//	public void setGetcolor(String getcolor) {
+//		this.getcolor = getcolor;
+//	}
+//	public String getGetsize() {
+//		return getsize;
+//	}
+//	public void setGetsize(String getsize) {
+//		this.getsize = getsize;
+//	}
+//	public String getState() {
+//		return state;
+//	}
+//	public void setState(String state) {
+//		this.state = state;
+//	}
+//	@Override
+//	public String toString() {
+//		return "InsertCartForm [goodsNo=" + goodsNo + ", getnumber=" + getnumber + ", imgurl=" + imgurl
+//				+ ", getgoodsname=" + getgoodsname + ", getprice=" + getprice + ", getAllprice=" + getAllprice
+//				+ ", getcolor=" + getcolor + ", getsize=" + getsize + ", state=" + state + "]";
+//	}
+//	
+//	
+
+}

e12ee52d6c33df8214bb8432ed7f2a02eb3d4126
JaLyJoker <1041551225@qq.com>
2019-11-29 22:27:43 +0800

    123

--- bshop/WebRoot/js/personInfo.js
+++ bshop/WebRoot/js/personInfo.js
@@ -201,7 +201,11 @@ $('.save').click(function(){
     				
         			if(obj.length > 0){
         				for(var i = 0; i < obj.length; i++){
-        					$(".user-right2").append("<ul class='addresslist'><li><div class='insertName'>"+obj[i].receiver+"&nbsp;&nbsp;&nbsp;&nbsp;"+obj[i].telephone+"</div><div class='insertPostcode'>邮编:"+obj[i].receiver+"</div><div class='insertMyaddress'>收货地址:"+obj[i].address+"</div><span class='binggou' onclick='changeBinggou(this)' >√</span><span class='redefult'>设为默认</span><div class='edit'>编辑</div><div class='del' onclick='delclick(this)' data-rid='"+obj[i].rid+"'>删除</div></li></ul>");
+        					if(obj[i].ischeck == "1"){
+        						$(".user-right2").append("<ul class='addresslist' data-ul='1'><li><div class='insertName'>"+obj[i].receiver+"&nbsp;&nbsp;&nbsp;&nbsp;"+obj[i].telephone+"</div><div class='insertPostcode'>邮编:"+obj[i].receiver+"</div><div class='insertMyaddress'>收货地址:"+obj[i].address+"</div><span class='binggou' onclick='changeBinggou(this)' >√</span><span class='redefult'>设为默认</span><div class='edit'>编辑</div><div class='del' onclick='delclick(this)' data-rid='"+obj[i].rid+"'>删除</div></li></ul>");
+        					}else{
+        						$(".user-right2").append("<ul class='addresslist'><li><div class='insertName'>"+obj[i].receiver+"&nbsp;&nbsp;&nbsp;&nbsp;"+obj[i].telephone+"</div><div class='insertPostcode'>邮编:"+obj[i].receiver+"</div><div class='insertMyaddress'>收货地址:"+obj[i].address+"</div><span class='binggou' onclick='changeBinggou(this)' >√</span><span class='redefult'>设为默认</span><div class='edit'>编辑</div><div class='del' onclick='delclick(this)' data-rid='"+obj[i].rid+"'>删除</div></li></ul>");
+        					}
                             $(".addresslist").css({
                                 "width": "1000px",
                                 "height": "120px",
@@ -247,11 +251,28 @@ $('.save').click(function(){
                             $(".redefult").css({
                                 "font-weight": "bolder",
                             })
+                            
         				}
     				}	
+//        			var len1 = $(".user-right2").children().length;
+//        			for(var i = 0;i < len1-2; i++){
+//        				console.log($(".user-right2").find("ul").get(i))
+//        				if($(".user-right2").find("ul").get(i).attr("data-ul") == "1"){
+//        					$(".user-right2").find("ul").get(i).parent().css({
+//        						"border" : "1px solid red"
+//        					})
+//        				}
+//        			}
+//        			for(var i = 0; i < obj.length; i++){
+//        				if(obj[i].ischeck == "1"){
+//        					
+//        				}
+//        			}
+        			
     			}else if(len > 2){
     				return;
     			}
+    			
     		}
     	})
     })
@@ -455,7 +476,6 @@ $('.save').click(function(){
     	var iphone = arr1[1];
     	var postcode = s2.slice(3);
     	var address = s3.slice(5);
-    	console.log(address)
     	var rid =$(obj).parent().find("div").eq(4).attr("data-rid");
     	
     	var mes = {
@@ -473,12 +493,19 @@ $('.save').click(function(){
     		url : "updateCheckAndInsert.do",
     		data : {"msg" : JSON.stringify(mes)},
     		success : function (re) {
-    		 	$(".binggou").css({
-    	    		"color" : "white"
-    	    	})
-    	    	$(obj).css({
-    	    		"color" : "red"
-    	    	})
+//    		 	$(".binggou").css({
+//    	    		"color" : "white"
+//    	    	})
+//    	    	$(obj).css({
+//    	    		"color" : "red"
+//    	    	})
+    			$(".addresslist").css({
+    				"border" : ""
+    			})
+    			$(obj).parent().parent().css({
+    				"border" : "1px solid red"
+    			})
+    			
     		}
     	})
     }
